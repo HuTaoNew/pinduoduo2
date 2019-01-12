d59ea5915e882be9e42ba4e7cd89a675aae08dd8
Hutao <hutao_hn@163.com>
2019-01-13 01:04:25 +0800

    添加5个一级路由文件

---
+++ src/views/Recommend/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>推荐</div>
+</template>
+
+<script>
+    export default {
+        name: "index.vue"
+    }
+</script>
+
+<style scoped lang="stylus" ref="stylesheet/stylus">
+
+</style>
