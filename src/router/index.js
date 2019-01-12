ca403b384de23e8e03634470c4cdddebbcbc32ce
Hutao <hutao_hn@163.com>
2019-01-13 01:12:42 +0800

    添加路由和修改组建名

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,26 @@ export default new Router({
       path: '/',
       name: 'home',
       component: () => import('@/views/Home')
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: () => import('@/views/Chat')
+    },
+    {
+      path: '/mine',
+      name: 'mine',
+      component: () => import('@/views/Mine')
+    },
+    {
+      path: '/recommend',
+      name: 'recommend',
+      component: () => import('@/views/Recommend')
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: () => import('@/views/Search')
     }
   ]
 })
