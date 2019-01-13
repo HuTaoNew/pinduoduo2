6840f84b5e0deffeca8b87390b8c6309ecf34c16
Hutao <hutao_hn@163.com>
2019-01-13 11:23:02 +0800

    使用第三方组件ly-tab

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,24 +1,41 @@
 <template>
   <div class="home">
-    
+    <ly-tab v-model="selectedId" :items="items" :options="options"></ly-tab>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-	
 export default {
-  name: 'home',
-  components: {
-    
-  }
-}
+  name: "home",
+  data() {
+    return {
+      selectedId: 0,
+      items: [
+        { label: "热门" },
+        { label: "服饰" },
+        { label: "鞋包" },
+        { label: "母婴" },
+        { label: "百货" },
+        { label: "食品" },
+        { label: "内衣" },
+        { label: "男装" },
+        { label: "电器" }
+      ],
+      options: {
+        activeColor: "#1d98bd"
+        // 可在这里指定labelKey为你数据里文字对应的字段
+      }
+    };
+  },
+  components: {}
+};
 </script>
 
 <style scoped>
-  .home {
-    width: 100%;
-    height: 100%;
-  }
+.home {
+  width: 100%;
+  height: 100%;
+}
 </style>
